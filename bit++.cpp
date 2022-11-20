89ba234251c22d3ab19006c87fe977aa80f9d690
Dalia Akter <117200227+dalia-akter926@users.noreply.github.com>
2022-11-20 10:22:53 +0600

    Add files via upload

---
+++ bit++.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a,i,x=0;
+    string s;
+    cin>>a;
+    cin>>s;
+    for(i=0;i<a;i++)
+
+    {
+        if(s=="x++")
+
+    {
+        x++;
+    }
+    else
+    {
+        x--;
+    }
+    }
+
+
+
+
+}
+
